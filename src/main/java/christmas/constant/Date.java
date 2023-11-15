600689679c70fb205c5aef77ab44ac2290b428c4
bbbang105 <2018111366@dgu.ac.kr>
2023-11-15 17:05:21 +0900

    feat : 12월 이벤트 날짜 정보 클래스 생성

---
+++ src/main/java/christmas/constant/Date.java
@@ -0,0 +1,9 @@
+package christmas.constant;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Date {
+    public static final List<Integer> WEEKDAY_DATES = Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
+    public static final List<Integer> SPECIAL_DISCOUNT_DATES = Arrays.asList(3, 10, 17, 24, 25, 31);
+}
